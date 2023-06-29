bc8f02307ecd721e1b539cc6f5bf853d04a8a23e
gio221 <barbosagiovana11@gmail.com>
2023-06-29 15:13:42 -0300

    Cadastro empresa

--- Oportunidades_Emprego/src/view/TelaListaVagas.java
+++ Oportunidades_Emprego/src/view/TelaListaVagas.java
@@ -96,6 +96,9 @@ public class TelaListaVagas implements ActionListener, ListSelectionListener {
 				break;
 		}
 	}
+	/**
+	 * Método que monstra em que o usuário clicou e determina o que vai acontecer na tela 
+	 */
 	public void actionPerformed(ActionEvent e) {
 		Object src = e.getSource();
 		
